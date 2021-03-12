cdb6a0f939753889d764665da5a95435b7fbf643
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-03-12 16:09:19 +0000

    Update: fix some problems with error correction.

--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -13,7 +13,9 @@ import pt.up.fe.comp.TestUtils;
 import pt.up.fe.specs.util.SpecsIo;
 
 public class ExampleTest {
-    // very good
+    //
+    // good
+    //
     @Test
     public void NachoTest() {
         String jmmCode = SpecsIo.read("test/nachotest.jmm");
@@ -92,7 +94,23 @@ public class ExampleTest {
         System.out.println(TestUtils.parse(jmmCode).getRootNode());
     }
 
+    @Test
+    public void TuringTest() {
+        System.out.println("Weird static import thingy.");
+        //var jmmCode = SpecsIo.getResource("fixtures/private/Turing.jmm");
+        //System.out.println(TestUtils.parse(jmmCode).getRootNode());
+    }
+
+    @Test
+    public void TicTacToeJavaTest() {
+        System.out.println("The grammar doesn't accept this file (it's java not jmm).");
+        //String jmmCode = SpecsIo.read("test/fixtures/public/java/TicTacToe.java");
+        //System.out.println(TestUtils.parse(jmmCode).getRootNode());
+    }
+
+    //
     // bad
+    //
     @Test(expected = Exception.class)
     public void BadNachoTest() {
         String jmmCode = SpecsIo.read("test/badnachotest.jmm");
@@ -105,10 +123,34 @@ public class ExampleTest {
         System.out.println(TestUtils.parse(jmmCode).getRootNode());
     }
 
+    // TODO problem is inside while loop so there are no exceptions thrown
+    @Test//(expected = Exception.class)
+    public void CompleteWhileTest() {
+        var jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/CompleteWhileTest.jmm");
+        System.out.println(TestUtils.parse(jmmCode).getRootNode());
+    }
+
     @Test//(expected = Exception.class)
     public void LengthErrorTest() {
-        // TODO problem is inside while loop so there is no exception thrown
         var jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/LengthError.jmm");
+        System.out.println(TestUtils.parse(jmmCode).getRootNode());
+    }
+
+    @Test//(expected = Exception.class)
+    public void MissingRightParTest() {
+        var jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/MissingRightPar.jmm");
+        System.out.println(TestUtils.parse(jmmCode).getRootNode());
+    }
+
+    @Test//(expected = Exception.class)
+    public void MultipleSequencialTest() {
+        var jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/MultipleSequential.jmm");
+        System.out.println(TestUtils.parse(jmmCode).getRootNode());
+    }
+
+    @Test//(expected = Exception.class)
+    public void NestedLoopTest() {
+        var jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/NestedLoop.jmm");
         System.out.println(TestUtils.parse(jmmCode).getRootNode());
     }
 }

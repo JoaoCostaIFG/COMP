be75824a362d7b498267e23ecf0e2bc65b6d81e4
Joao Bispo <jbispo@fe.up.pt>
2021-04-13 14:43:34 +0100

    Updates execution scripts (sh and .bat)

--- comp2021-1c.bat
+++ comp2021-1c.bat
@@ -1,3 +1,3 @@
 @echo off
 
-java -cp "%~dp0comp2021-1c.jar;%~dp0libs/utils.jar;%~dp0libs/gson-2.8.2.jar" %*
+java -cp "%~dp0comp2021-1c.jar;%~dp0libs/utils.jar;%~dp0libs/gson-2.8.2.jar;%~dp0libs/ollir.jar;%~dp0libs/jasmin.jar" %*
